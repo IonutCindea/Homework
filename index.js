6960fbcc021c1fbf24fd95a4d96840aebddd9c90
ionut <ionutkill@gmail.com>
2023-05-20 23:31:35 +0300

    tema java script

---
+++ index.js
@@ -0,0 +1,56 @@
+
+ function inpartie2(num){
+console.log (num/10===100)
+
+}
+ 
+inpartie2 (1000)
+
+function inpartire (num){
+    console.log (num/9===23)
+  }
+
+ inpartire (207)
+
+ 
+
+let obj = {
+
+     firstName: "Zoli",
+     locatie: "Ungaria",
+     loculDeMunca: "Constructor",
+     numarCopii: "3",
+
+}
+let sentence = `Vei fi un ${obj.loculDeMunca} in ${obj.locatie} casatorit cu ${obj.firstName} si vei avea ${obj.numarCopii} copii. `
+ 
+    console.log (sentence)
+
+
+    let ziua = 1; 
+    switch(ziua){ 
+        case 1: 
+        console.log ("Luni")
+        break;
+        case 2: 
+        console.log ("Marti")
+        break;
+        case 3: 
+        console.log ("Miercuri")
+        break;
+        case 4: 
+        console.log ("Joi")
+        break;
+     
+        console.log(ziua)
+
+
+    }
+   
+    ziua = 5
+    ziua === 5 ? console.log ("Vineri") : console.log ("Sambata") 
+
+    
+   
+
+
